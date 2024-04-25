37828d2c655fc433a41e80f42c8f5109aa0db290
Riwaj9786 <riwajbhurtel9786@gmail.com>
2024-04-25 22:01:22 +0545

    First Commit

---
+++ siddh_app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from siddh_app.models import Student, Faculty
+
+class StudentForm(forms.ModelForm):
+
+    class Meta():
+        model = Student
+        fields = ('batch_year', 'first_name', 'middle_name', 'last_name', 'do_birth', 'gender', 'faculty', 'semester')
+
+        widgets = {
+            'date_of_birth': forms.DateInput(attrs={'type': 'date'}),  # Use DateInput widget with type='date'
+            'text': forms.TextInput(attrs={'class': 'textinputclass'}),
+        }
